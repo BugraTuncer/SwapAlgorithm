5a13210f410ee1b0b5103469ebe5ac682620cf3a
Buğra Tunçer <>
2019-05-08 13:41:54 +0300

    first commit

---
+++ SwapAlgorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SwapAlgorithm
+//
+//  Created by Buğra Tunçer on 8.05.2019.
+//  Copyright © 2019 Buğra Tunçer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
